438dd0926dce08a9fd381fc36bb1766dd1510abb
Yudin Nikita <yudin.nc@gmail.com>
2020-03-29 19:00:37 +0500

    blog

---
+++ blog.php
@@ -0,0 +1,188 @@
+
+<?php include('templates/header_page.php'); ?>
+		<section class="inner-banner">
+			<div class="container">
+				<h2 class="inner-banner__title">Press</h2><!-- /.inner-banner__title -->
+				<ul class="list-unstyled thm-breadcrumb">
+					<li><a href="index.html">Home</a></li>
+					<li>Press</li>
+				</ul><!-- /.thm-breadcrumb -->
+			</div><!-- /.container -->
+		</section><!-- /.inner-banner -->
+		        <section class="blog-one blog-one__grid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-3.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                    <a href="blog-post.html" class="blog-one__share"><i class="fa fa-share-alt"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-4.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-4.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-3.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-4.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-3.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-4.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-4.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                    <div class="col-lg-4">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <div class="blog-one__image-inner">
+                                    <img src="assets/images/blog/blog-1-3.jpg" alt="Awesome Image" />
+                                    <a href="blog-post.html" class="blog-one__image-link"><i class="fa fa-link"></i></a>
+                                </div><!-- /.blog-one__image-inner -->
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <div class="blog-one__date"><span>29</span>Nov</div><!-- /.blog-one__date -->
+                                <h3 class="blog-one__title"><a href="blog-post.html">Tips for the Security Regulators The ‘Inside Story’</a></h3><!-- /.blog-one__title -->
+                                <p class="blog-one__text">Miventore veritatis et quasi architec beat vitae dicta sunt explicabo nem enim cone quuntur magni dolores eos.</p><!-- /.blog-one__text -->
+                                <div class="blog-one__bottom">
+                                    <a class="blog-one__link" href="blog-post.html"><span>More Details</span> <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div><!-- /.blog-one__bottom -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                </div><!-- /.row -->
+                <div class="post-pagination">
+                    <a class="post-pagination__prev" href="#">Prev</a>
+                    <a href="#">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                    <a class="post-pagination__next" href="#">Next</a>
+                </div><!-- /.post-pagination -->
+            </div><!-- /.container -->
+        </section><!-- /.blog-one -->
+<?php include('templates/footer.php'); ?>
